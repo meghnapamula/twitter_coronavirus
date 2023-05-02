fa0373e8d954e06824d9c42618828da14184f99c
Meghna Pamula <mpamula25@cmc.edu>
2023-05-01 22:45:48 -0700

    visualize

--- src/visualize.py
+++ src/visualize.py
@@ -12,6 +12,9 @@ args = parser.parse_args()
 import os
 import json
 from collections import Counter,defaultdict
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # open the input path
 with open(args.input_path) as f:
@@ -26,3 +29,30 @@ if args.percent:
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
 for k,v in items:
     print(k,':',v)
+
+# keys and values
+top_items = items[:10] 
+key_range = [item[0] for item in top_items]
+val_range = [item[1] for item in top_items]
+keys = key_range[::-1]
+values = val_range[::-1]
+
+# bar graph
+plt.bar(range(len(keys)), values)
+plt.xticks(range(len(keys)), keys)
+
+# set labels - title and axis
+if args.input_path[-1] == 'g':
+    plt.xlabel('Language')
+else:
+    plt.xlabel('Country')
+if args.percent:
+    plt.ylabel('Percent of Total')
+else:
+    plt.ylabel('Tweet Volume')
+
+# save PNG file of graph
+if args.input_path[-1] == 'g':
+    plt.savefig(args.key[1:] + '_lang.png')
+else:
+    plt.savefig(args.key[1:] + '_country.png')
